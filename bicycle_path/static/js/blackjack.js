efa7a131b748a867a2328eccc5f3efc5776f04d9
adoc <adoc@code.webmob.net>
2014-10-19 00:58:47 -0700

    Beginning backbone.js implementation.

--- bicycle_path/static/js/blackjack.js
+++ bicycle_path/static/js/blackjack.js
@@ -1,14 +1,11 @@
-/*  blackjack.js
-    Blackjack Game.
+/*  
     (c) 2010 - 2014 C. Nicholas Long
 */
 
-define(['jquery', 'underscore', 'site', 'config', 'bootstrap'],
-    function($, _, Site, Config) {
-        return {
-            boot: function() {
-                console.log(Config.engine_url('12345', 'bet'));
-            }
-        };
+define(['underscore', 'config', 'views'],
+    function(_, Config, Views) {
+
+        return {};
+
     }
 );
